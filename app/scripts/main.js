fc8a478ec067e7763176b87febfc1bafbdcd6411
Randy L <randay@gmail.com>
2013-08-04 16:55:35 -0400

    this is looking better

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,7 +38,13 @@ require([
     }
   });
 
-  window.Img = Backbone.Model.extend();
+  window.Img = Backbone.Model.extend({
+    // initialize: function () {
+    //   this.on('change:isCurrent', function () {
+
+    //   })
+    // }
+  });
 
   window.ImageList = Backbone.Collection.extend({
     model: Img,
@@ -65,48 +71,88 @@ require([
 
     el: 'ul',
 
+    events: {
+      'click img': 'jumpTo'
+    },
+
     initialize: function (options) {
       console.log(options.models);
-      //console.log(options.models.toJSON());
+      this.current = 0;
+      this.numImages = options.models.length;
       this.$el = $('.image-list');
       this.model = options.models;
+      // setup a highlight/outline
     },
 
     render: function () {
       this.$el.html(this.template({'images': this.model}));
+      $('li:first', this.el).css('outline', 'solid 2px yellow');
       return this;
+    },
+
+    back: function () {
+      this.current === 0 ? this.numImages - 1 : this.current--;
+      $('li', this.el).css('outline', '');
+      $('li', this.el).eq(this.current).css('outline', 'solid 2px yellow');
+    },
+
+    forward: function () {
+      this.current === this.numImages - 1 ? 0 : this.current++;
+      $('li', this.el).css('outline', '');
+      $('li', this.el).eq(this.current).css('outline', 'solid 2px yellow');
+    },
+
+    jumpTo: function (evt) {
+      var index = $(evt.target).parent().index();
+      // HACK: fix this
+      gallery.jumpTo(index);
     }
   });
 
   window.GalleryApp = Backbone.View.extend({
     el: $('#gallery'),
 
+    events: {
+      'click .arrow-left': 'back',
+      'click .arrow-right': 'forward'
+    },
+
     initialize: function () {
-      var imgs = [];
-      imgs.push(new Img({'path': '/images/image1.png', 'thumbPath': '/images/thumbnails/image1_t.png'}));
-      imgs.push(new Img({'path': '/images/image2.png', 'thumbPath': '/images/thumbnails/image2_t.png'}));
-      imgs.push(new Img({'path': '/images/image3.png', 'thumbPath': '/images/thumbnails/image3_t.png'}));
-
-      this.images = new ImageList(imgs);
-
-      var img = new ImageView({ model: imgs[0] });
-      this.$el.append(img.render().el);
-      // img = new ImageView({ model: imgs[1] });
-      // this.$el.append(img.render().el);
-      // img = new ImageView({ model: imgs[2] });
-      // this.$el.append(img.render().el);
-
-      var listView = new ImageStripView(this.images);
-      $('.image-list').append(listView.render().el);
-
-
       this.current = 0;
+      this.numImages = 3;
+      this.imgs = [];
+      this.imgs.push(new Img({'path': '/images/image1.png', 'thumbPath': '/images/thumbnails/image1_t.png'}));
+      this.imgs.push(new Img({'path': '/images/image2.png', 'thumbPath': '/images/thumbnails/image2_t.png'}));
+      this.imgs.push(new Img({'path': '/images/image3.png', 'thumbPath': '/images/thumbnails/image3_t.png'}));
+
+      this.imageList = new ImageList(this.imgs);
+
+      this.imageView = new ImageView({ model: this.imgs[this.current] });
+      this.$el.append(this.imageView.render().el);
+
+      this.listView = new ImageStripView(this.imageList);
+      $('.image-list').append(this.listView.render().el);
 
       this.render();
     },
     render: function () {
 
       return this;
+    },
+    back: function () {
+      console.log('in GalleryApp.back!!!');
+      this.current === 0 ? this.numImages - 1 : this.current--;
+      this.listView.back();
+    },
+    forward: function () {
+      console.log('in GalleryApp.forward!!!');
+      this.current === this.numImages - 1 ? 0 : this.current++;
+      this.listView.forward();
+    },
+    jumpTo: function (index) {
+      console.log('in GalleryApp.jumpTo()');
+      this.current = index;
+      //this.images.jumpTo(index);
     }
   });
 
